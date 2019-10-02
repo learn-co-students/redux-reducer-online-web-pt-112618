1e95561f2e6ca5b8542c4d3c9f795184204dfa2c
DimaSamo <dimasamo96@gmail.com>
2019-10-02 17:07:24 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,22 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            let newState = {...state}
+            newState.friends.push(action.friend)
+            return newState
+            
+        case 'REMOVE_FRIEND':
+            const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+            return (
+                {...state,
+                    friends: [
+                    ...state.friends.slice(0, removalIndex),
+                    ...state.friends.slice(removalIndex + 1)
+                    ]
+                }
+            )
+    
+        default:
+            return {friends: state.friends};
+    }
 }
